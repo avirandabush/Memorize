a2ddf47995a8afdfd0dd97068a07a90af74a1478
aviran <avirand@balink.net>
2025-05-13 15:01:21 +0300

    separate views into vars.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,35 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojies = ["🕸️", "💀", "👻", "🎃", "😈", "👹", "👿", "🕷️", "🕸️", "💀", "👻", "🎃", "😈", "👹", "👿", "🕷️", "🎃", "😈", "👹", "👿", "🕷️"]
+    @State var count: Int = 4
+
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
-            CardView(isFaceUp: true)
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            countAdjustmentButtons
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 120))]) {
+            ForEach(0..<count, id: \.self) { emoji in
+                CardView(content: emojies[emoji])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+    }
+    
+    var countAdjustmentButtons: some View {
+        HStack {
+            substructButton
+            Spacer()
+            addButton
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var substructButton: some View {
+        countAdjustmentButton(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var addButton: some View {
+        countAdjustmentButton(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    func countAdjustmentButton(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                count += offset
+        }) {
+            Image(systemName: symbol)
+        }
+        .disabled(count + offset < 2 || count + offset > emojies.count)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
+    let content: String
     
     var body: some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundStyle(Color(.white))
-                RoundedRectangle(cornerRadius: 20)
+            let cardBase = RoundedRectangle(cornerRadius: 12)
+            
+            Group {
+                cardBase
+                    .fill(Color(.white))
+                cardBase
                     .strokeBorder(lineWidth: 3)
                     .foregroundColor(.cyan)
-                Text("😳")
+                Text(content)
                     .font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            cardBase
+                .opacity(isFaceUp ? 0 : 1)
         }
         .foregroundStyle(Color(.cyan))
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
